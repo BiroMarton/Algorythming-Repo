37d5211af3a00c4980a57084f9e7441536b60286
BiroMarton <rezoner9@gmail.com>
2017-09-27 10:52:23 +0200

    Completed 2 more tasks

--- AlgorythmingApp/AlgorythmingApp/Program.cs
+++ AlgorythmingApp/AlgorythmingApp/Program.cs
@@ -166,22 +166,103 @@ namespace AlgorythmingApp
             return result;
         }
 
+        public T[,] ChunkMonkey<T>(T[] arr, int split)
+        {
+            T[,] resultArr;
+            int counter = 0;
+            int counter2 = 0;
+            if (arr.Count() % split == 0)
+            {
+                resultArr = new T[arr.Count() / split, split];
+            }
+            else
+            {
+                resultArr = new T[arr.Count() / split +1, split];
+
+            }
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                resultArr[counter,counter2] = arr[i];
+                counter2++;
+                if (counter2 == split)
+                {
+                    counter++;
+                    counter2 = 0;
+                }
+                
+            }
+
+            return resultArr;
+
+        }
+
+        public static void Print2DArray<T>(T[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public static void PrintOutArray<T>(T[] arr)
+        {
+            foreach (T item in arr)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+        }
+
+        public T[] Slasher<T>(T[] list, int num)
+        {
+            T[] result;
+            if (num > list.Count())
+            {
+                return result = new T[0];
+            }
+            result = new T[list.Count() - num];
+            for (int i = 0; i < result.Count(); i++)
+            {
+                result[i] = list[i + num];
+
+            }
+            return result;
+
+        }
+
         static void Main(string[] args)
         {
             int[,] myArr = new int[4, 4] { { 4, 5, 1, 3 }, { 13, 24, 18, 26 }, { 32, 35, 37, 39 }, { 1000, 1001, 857, 1 } };
+            string[] myStrArr = new string[] {"a","b","c","d","e","f","g" };
+            int[] myIntArr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
            Program p = new Program();
-            int i = 1;
+
             Console.WriteLine(p.ReverseString("1234567890"));
+
             Console.WriteLine(p.FactorizeNumber(5).ToString());
+
             Console.WriteLine(p.IsPalindrome("My age is 0, 0 si ega ym."));
+
             Console.WriteLine(p.LongestWord("How do I remove all non alphanumeric characters from a string except dash and space characters?"));
+
             Console.WriteLine(p.Capitalize("union of soviet socialist republics"));
+
             for (int j = 0; j < 4; j++)
             { Console.WriteLine(p.LargestNumbers(myArr)[j]); }
             
             Console.WriteLine(p.ConfirmEnding(" Meet Aspen the mountain pup, a 4-year-old Golden Retriever who is all about enjoying the fresh air and exploring the world's natural wonder", "der"));
+
             Console.WriteLine(p.RepeatString("Abc", 6));
+
             Console.WriteLine(p.Turncate("Kiskutya, nagykutya nem ugat hiába.",11));
+
+            Print2DArray(p.ChunkMonkey(myStrArr,2));
+
+            PrintOutArray(p.Slasher(myIntArr, 2));
+
             Console.ReadKey();
 
               
